b9ccc46776c417cfd7c2a912dec450ff9a272b4b
Sharynne Azhar <sharynneazhar@gmail.com>
2016-12-16 12:55:13 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# React Jeopardy
+
+Forked from [ccoenraets](https://ccoenraets.github.io/react-trivia/).
+
+## Build the App
+
+If you want modify the app and create you own build:
+
+1. Clone this repo
+    ```
+    git clone https://github.com/ccoenraets/react-trivia
+    ```
+
+1. Install the dependencies
+    ```
+    npm install
+    ```
+
+1. Modify the app in the **/app** folder
+
+1. Build the app
+    ```
+    npm run webpack
+    ```
+
+## Credits
+Initially inspired by [Ember Jeopardy](https://github.com/machty/ember-jeopardy)
+
+## License
+
+- App: MIT
+- Music: [Orange Free Sounds](http://www.orangefreesounds.com/jeopardy-theme-song/)
---
+++ data.js
@@ -0,0 +1,122 @@
+var data = [
+  {
+    "category": "Jesus' Birth",
+    "questions": [
+      {
+        "points": 100,
+        "question": "Jesus was laid in this item at his birth",
+        "answer": "What is a manger?"
+      },
+      {
+        "points": 200,
+        "question": "The meaning of the word \"Immanuel\".",
+        "answer": "What is \"God With Us\"?"
+      },
+      {
+        "points": 300,
+        "question": "The country Jesus' family escaped to after they received a death threat over Jesus.",
+        "answer": "Where is Egypt?"
+      },
+      {
+        "points": 400,
+        "question": "The city where Jesus was brought up as a child.",
+        "answer": "Where is Nazareth?"
+      },
+      {
+        "points": 500,
+        "question": "The Emperor at the time of Jesus' birth.",
+        "answer": "Who is Augustus?"
+      }
+    ]
+  },
+  {
+    "category": "Mary & Joseph",
+    "questions": [
+      {
+        "points": 100,
+        "question": "Jesus' father.",
+        "answer": "Who is God?"
+      },
+      {
+        "points": 200,
+        "question": "Joseph planned to do this when he found out that Mary was pregnant.",
+        "answer": "What is to quietly divorce her?"
+      },
+      {
+        "points": 300,
+        "question": "The person Mary went to visit soon after the angel's visit.",
+        "answer": "Who is Elizabeth?"
+      },
+      {
+        "points": 400,
+        "question": "Joseph and Mary went to Bethlehem to do this.",
+        "answer": "What is for Joseph to register for the census?"
+      },
+      {
+        "points": 500,
+        "question": "The sacrifice Mary and Joseph offered when the days of purification were complete after Jesus' birth.",
+        "answer": "What are two turtledoves or pigeons?"
+      }
+    ]
+  },
+  {
+    "category": "The Wise Men",
+    "questions": [
+      {
+        "points": 100,
+        "question": "The Wise Men came from the _____.",
+        "answer": "Where is the east?"
+      },
+      {
+        "points": 200,
+        "question": "The gifts the Wise Men brought to Jesus.",
+        "answer": "What are gold, frankincense, and myrrh?"
+      },
+      {
+        "points": 300,
+        "question": "How did the Wise Men know that Jesus was born?",
+        "answer": "What is they saw a star in the East?"
+      },
+      {
+        "points": 400,
+        "question": "He asked the wise men where Jesus was.",
+        "answer": "Who is Herod?"
+      },
+      {
+        "points": 500,
+        "question": "How many Wise Men came to visit Jesus?",
+        "answer": "What is it doesn't say?"
+      }
+    ]
+  },
+  {
+    "category": "Christmas Carols",
+    "questions": [
+      {
+        "points": 100,
+        "question": "The tree mentioned in \"The 12 Days of Christmas.\"",
+        "answer": "What is a pear tree?"
+      },
+      {
+        "points": 200,
+        "question": "How many times does Santa check his list?",
+        "answer": "What is twice?"
+      },
+      {
+        "points": 300,
+        "question": "Name at least 4 of Santa's reindeer.",
+        "answer": "Who are Dasher, Dancer, Prancer, Vixen, Comet, Cupid, Donner, Blitzen, or Rudolph?"
+      },
+      {
+        "points": 400,
+        "question": "\"What a bright time, it's the right time, to rock the night away\" comes from this song.",
+        "answer": "What is Jingle Bell Rock?"
+      },
+      {
+        "points": 500,
+        "question": "The number of gifts would you receive if you received all the gifts in the song \"The 12 Days of Christmas.\"",
+        "answer": "What is 364?"
+      }
+    ]
+  }
+];
